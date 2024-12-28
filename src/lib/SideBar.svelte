5e49afb01e3a0872546a1760f8aefba75ab913fb
Abolfazl N.Ranjbar <98ranjbar@gmail.com>
2024-12-28 16:06:35 +0330

    fix sync with storage

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -2,9 +2,13 @@
 	import { fade, fly } from 'svelte/transition';
 	import { notes, type Note } from './hooks/notes.svelte';
 	$effect(() => {
-		$inspect(notes.items);
-		localStorage.setItem('notes', JSON.stringify(notes.items));
+		if (localStorage.getItem('notes') && !notes.items.length) {
+			notes.items = JSON.parse(localStorage.getItem('notes') as string);
+		}
 	});
+	const handleSaveNotes = () => {
+		localStorage.setItem('notes', JSON.stringify(notes.items));
+	};
 	const handleSelectNote = (id: Note['id']) => {
 		notes.selectedNoteId = id;
 	};
@@ -61,43 +65,45 @@
 </script>
 
 {#if notes.showNotes}
-	<div class="fixed inset-0 mt-16 backdrop-blur-sm"></div>
 	<aside
-		transition:fly|global={{ x: -100, y: 0, duration: 300 }}
+		transition:fly|global={{ x: -100, y: 0, duration: 500 }}
 		class:fixed={notes.showNotes}
-		class="left-0 z-10 mt-16"
+		class="scrollbar left-0 z-10 mt-16 h-screen w-screen overflow-y-auto bg-slate-100"
 	>
 		<div
-			class="scrollbar flex h-screen w-screen flex-col gap-2 overflow-y-auto bg-slate-100"
+			class="container mx-auto grid grid-cols-2 grid-rows-[repeat(1fr)] gap-2"
 		>
-			<div class="flex flex-col items-center justify-between space-y-2 p-2">
-				{#each notes.items as note, index}
-					<!-- svelte-ignore a11y_click_events_have_key_events -->
-					<!-- svelte-ignore a11y_no_static_element_interactions -->
-					<div
-						transition:fade|global
-						onclick={() => handleSelectNote(note.id)}
-						class:flex-1={Boolean(note.id === notes.selectedNoteId)}
-						class={`h-28 w-full cursor-pointer rounded-md border border-gray-600/40 px-2 py-1 text-gray-700 transition-colors`}
-					>
-						<div class="flex items-start justify-between">
-							<div class="flex w-full flex-col items-start gap-2">
-								<h3
-									contenteditable
-									bind:innerText={note.title}
-									class="px-2 font-medium"
-								>
-									{note.title}
-								</h3>
-								<p
-									contenteditable
-									class:truncate={!Boolean(note.id === notes.selectedNoteId)}
-									bind:innerText={note.content}
-									class=" max-w-full rounded-md border-gray-600/40 px-2 py-1"
-								>
-									{note.content}
-								</p>
-							</div>
+			{#each notes.items as note, index}
+				<div
+					transition:fade|global
+					class:flex-1={Boolean(note.id === notes.selectedNoteId)}
+					class={` w-full cursor-pointer rounded-md border border-gray-600/40 px-2 py-1 text-gray-700 transition-colors`}
+				>
+					<div class="">
+						<div class="flex w-full flex-col items-start gap-2">
+							<h3
+								contenteditable
+								bind:innerText={note.title}
+								class="px-2 font-medium"
+							>
+								{note.title}
+							</h3>
+							<p
+								contenteditable
+								class:truncate={!Boolean(note.id === notes.selectedNoteId)}
+								bind:innerText={note.content}
+								class=" max-w-full rounded-md border-gray-600/40 px-2 py-1"
+							>
+								{note.content}
+							</p>
+						</div>
+						<div>
+							<button
+								onclick={() => handleSelectNote(note.id)}
+								class="p-0 text-sm text-red-500 hover:text-red-700"
+							>
+								✏️</button
+							>
 							<button
 								onclick={(e) => {
 									e.stopPropagation();
@@ -111,25 +117,28 @@
 							</button>
 						</div>
 					</div>
-				{/each}
-				<div class="flex h-36 w-full flex-col gap-2">
-					<button
-						class="hover:scale-10 bg-green-500 text-white"
-						onclick={handleExportNotes}>Export Notes</button
-					>
-					<label
-						class="w-full cursor-pointer rounded-md bg-blue-500 px-4 py-2 text-center text-white transition-colors hover:bg-blue-600"
-					>
-						Import Notes
-						<input
-							type="file"
-							accept=".json"
-							onchange={handleImportNotes}
-							class="hidden"
-						/>
-					</label>
-					<!-- <button class="bg-gray-500 text-white"> Import Notes</button> -->
 				</div>
+			{/each}
+			<div class="flex h-36 w-1/4 flex-col gap-2">
+				<button
+					class="hover:scale-10 bg-green-500 text-white"
+					onclick={handleSaveNotes}>Save Notes</button
+				>
+				<button
+					class="hover:scale-10 bg-purple-700 text-white"
+					onclick={handleExportNotes}>Export Notes</button
+				>
+				<label
+					class="w-full cursor-pointer rounded-md bg-blue-500 px-4 py-2 text-center text-white transition-colors hover:bg-blue-600"
+				>
+					Import Notes
+					<input
+						type="file"
+						accept=".json"
+						onchange={handleImportNotes}
+						class="hidden"
+					/>
+				</label>
 			</div>
 		</div>
 	</aside>
